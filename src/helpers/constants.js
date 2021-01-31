dbcf16c9c70172a7b669811149b3ebda07556959
Abdullahi Oladosu <ayobami.oladosu@gmail.com>
2021-01-31 07:06:26 +0100

    Changed BASE_URL to localhost for running BE & FE on the same machine

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://192.168.43.22:5000";
+// export const BASE_URL = "http://192.168.43.22:5000";
+export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "http://192.168.0.107:5000";
